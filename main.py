ccd3c0bd994211d648b02c17939fb88f98a8a7bc
mveco <marijavecovska99@gmail.com>
2023-09-20 22:49:00 +0200

    "renamed the dictionary generator"

--- main.py
+++ main.py
@@ -26,84 +26,92 @@ def find_node_shapes(shape):
 
 
 # given a rdf list, it returns a python list containing the items in the RDF list.
-def get_values_from_list(start, shape):
-    items = []
-    first = next(shape.objects(start, RDF.first))
-    items.append(first)
-    rest = next(shape.objects(start, RDF.rest))
+def get_list_from_shacl_list(start, shapes_graph):
+    _list = []
+    first = next(shapes_graph.objects(start, RDF.first))
+    _list.append(first)
+    rest = next(shapes_graph.objects(start, RDF.rest))
     if rest == RDF.nil:
-        return items
+        return _list
     else:
-        return items + get_values_from_list(rest, shape)
+        return _list + get_list_from_shacl_list(rest, shapes_graph)
 
 
 # given a rdf list of shapes, it returns a python list containing the dictionaries for each shape in the rdf list
-def get_values_from_property_list(start, shape):
+def get_dict_list_from_shacl_list(start, shapes_graph, property_pair_constraint_components):
     items = []
-    first = next(shape.objects(start, RDF.first))
-    items.append(subject_to_dictionary(first, shape, []))
-    rest = next(shape.objects(start, RDF.rest))
+    first = next(shapes_graph.objects(start, RDF.first))
+    items.append(shape_to_dictionary(first, shapes_graph, property_pair_constraint_components))
+    rest = next(shapes_graph.objects(start, RDF.rest))
     if rest == RDF.nil:
         return items
     else:
-        return items + get_values_from_property_list(rest, shape)
+        return items + get_dict_list_from_shacl_list(rest, shapes_graph, property_pair_constraint_components)
 
 
 # given a subject, build a dictionary of all the triples it participates in
-def subject_to_dictionary(subject, shape, property_pair_constraint_components_parent):
-    triples = shape.triples((subject, None, None))
+def shape_to_dictionary(shape, shapes_graph, property_pair_constraint_components_parent):
+    triples = shapes_graph.triples((shape, None, None))
     # all property pair constraint components that are a sh:property for this subject are stored in here
     property_pair_constraint_components = []
     # a dictionary containing all triples for the current subject
-    properties = {}
-    subject_dictionary = {}
+    sh_properties = {}
+    shape_dictionary = {}
     for s, p, o in triples:
         if p == SH.property:
             # if the property is a SH:property, then the object is a blank node, which st the same time is a subject in a triple with SH:path property.
             # the subject in the second triple will be the key in the dictionary, and the remaining properties and objects will be added in a dictionary
-            property_path = next(shape.objects(o, SH.path))
+            property_path = next(shapes_graph.objects(o, SH.path))
             # if there is allready an entry for this property, upadete its dict instead of overriding it
-            property_dict = properties.get(property_path, {})
-            property_dict.update(subject_to_dictionary(o, shape, property_pair_constraint_components))
-            properties[property_path] = property_dict
-            # in the property_dictionary will be kept all of the data for the new path
+            property_dict = sh_properties.get(property_path, {})
+            property_dict.update(shape_to_dictionary(o, shapes_graph, property_pair_constraint_components))
+            sh_properties[property_path] = property_dict
+            # in the property_dictionary will be kept all the data for the new path
         elif p == URIRef(SH + "in"):
-            subject_dictionary[p] = get_values_from_list(o, shape)
+            shape_dictionary[p] = get_list_from_shacl_list(o, shapes_graph)
         elif p == URIRef(SH + "or") or p == URIRef(SH + "and") or p == URIRef(SH + "xone") or p == URIRef(SH + "not"):
-            subject_dictionary[p] = get_values_from_property_list(o, shape)
+            shape_dictionary[p] = get_dict_list_from_shacl_list(o, shapes_graph, property_pair_constraint_components)
         else:
             # add all dicts that have a Property Pair Constraint Component to be changed additionaly
             if p == SH.lessThan or p == SH.lessThanOrEquals or p == SH.equals or p == SH.disjoint:
-                property_pair_constraint_components_parent.append(subject_dictionary)
-            subject_dictionary[p] = o
+                property_pair_constraint_components_parent.append(shape_dictionary)
+            shape_dictionary[p] = o
     # add proprties dictionary only if the node has properties
-    if bool(properties):
-        subject_dictionary["properties"] = properties
+    if bool(sh_properties):
+        shape_dictionary["properties"] = sh_properties
 
     # fixes the dependencies caused by Property Pair Constraint Components
     for d in property_pair_constraint_components:
         # if there is a sh:equals predicate in this component, and it doesnt exist in the dict, add it
         equals = d.get(SH.equals)
-        if equals and not properties.get(SH.equals):
-            properties[equals] = {SH.path: equals}
+        if equals and not sh_properties.get(SH.equals):
+            sh_properties[equals] = {SH.path: equals}
 
         # if there is a sh:disjoint predicate in this component, and it doesnt exist in the dict, add it
         disjoint = d.get(SH.disjoint)
-        if disjoint and not properties.get(SH.disjoint):
-            properties[disjoint] = {SH.path: disjoint}
+        if disjoint and not sh_properties.get(SH.disjoint):
+            sh_properties[disjoint] = {SH.path: disjoint}
 
         # if there is a sh:lessThan predicate in this component, and it doesnt exist in the dict, add it.
         # if the property with sh:lessThan constraint has a sh:minInclusive/sh:minExclusive constraint,
         # the constraint should be added to the property that it points to.
         less_than = d.get(SH.lessThan)
-        if less_than and not properties.get(less_than):
-            properties[less_than] = {SH.path: less_than}
+        if less_than and not sh_properties.get(less_than):
+            sh_properties[less_than] = {SH.path: less_than}
             min_constraint = d.get(SH.minInclusive, d.get(SH.minExclusive))
             if min_constraint:
-                ltproperty = properties.get(less_than)
-                ltproperty[SH.minInclusive] = min_constraint
+                lt_property = sh_properties.get(less_than)
+                lt_property[SH.minInclusive] = min_constraint
 
-    return subject_dictionary
+        less_than_or_equals = d.get(SH.lessThanOrEquals)
+        if less_than_or_equals and not sh_properties.get(less_than_or_equals):
+            sh_properties[less_than_or_equals] = {SH.path: less_than_or_equals}
+            min_constraint = d.get(SH.minInclusive, d.get(SH.minExclusive))
+            if min_constraint:
+                ltoe_property = sh_properties.get(less_than_or_equals)
+                ltoe_property[SH.minExclusive] = min_constraint
+
+    return shape_dictionary
 
 
 # generates a random value based on the SH:datatype
@@ -295,7 +303,7 @@ def generate_dictionary(shape):
     nodes = find_node_shapes(shape)
     dictionary = {}
     for n in nodes:
-        dictionary[n] = subject_to_dictionary(n, shape, [])
+        dictionary[n] = shape_to_dictionary(n, shape, [])
     return dictionary
 
 
