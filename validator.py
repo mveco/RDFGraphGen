45f218ff47d06c4743af00cf0a0625765c93efea
mveco <marijavecovska99@gmail.com>
2023-09-22 10:20:10 +0200

    "added the data folder(containing files for testing) and the validator"

---
+++ validator.py
@@ -0,0 +1,20 @@
+from pyshacl import validate
+
+
+shapes_file = "data//or_example.ttl"
+shapes_file_format = 'turtle'
+
+data_file = "data//or_sol.ttl"
+data_file_format = 'turtle'
+
+conforms, v_graph, v_text = validate(data_file, shacl_graph=shapes_file,
+                                     data_graph_format=data_file_format,
+                                     shacl_graph_format=shapes_file_format,
+                                     inference='rdfs', debug=False,
+                                     serialize_report_graph=True)
+print(conforms)
+print(v_graph)
+print(v_text)
+
+# minInclusive, minExclusive, maxInclusive, maxExclusive work for all datatypes.
+#
