f2ab1c7785787ba9b4c3c7f5afef771c9441e296
Marija <marijavecovska99@gmail.com>
2024-01-30 23:47:20 +0100

    "Added a script.py file that can be used to rud the app from terminal. "

---
+++ script.py
@@ -0,0 +1,16 @@
+import argparse
+from rdf_graph_generator import *
+
+
+def main():
+    parser = argparse.ArgumentParser(description="CLI for processing two files.")
+    parser.add_argument("file1", help="Path to the first input file")
+    parser.add_argument("file2", help="Path to the second input file")
+    parser.add_argument("instance_no", help="Number of instances that should be generated")
+
+    args = parser.parse_args()
+    create_rdf_examples(args.file1, args.file2, int(args.instance_no))
+
+
+if __name__ == "__main__":
+    main()
