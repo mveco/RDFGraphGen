04c05c4a4d18644511fe0bcb0f1399daac61fe39
Marija <marijavecovska99@gmail.com>
2024-01-31 16:15:42 +0100

    "added setup file"

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='rdf_shacl_generator',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'genrdf = rdf_shacl_generator.script:main',
+        ],
+    },
+)
