ff19bd67ac12aba7c2fb71189608336ca64b973d
Marija <marijavecovska99@gmail.com>
2024-02-28 11:28:20 +0100

    "Added new version number"

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='rdf_shacl_generator',
-    version='0.1.0',
+    version='1.0.0',
     packages=find_packages(),
     package_data={'rdf_shacl_generator': ['datasets/*.csv']},
     install_requires=requirements,
