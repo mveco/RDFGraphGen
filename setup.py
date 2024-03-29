dd31d8caed3bac080108f92bd7d19e8db1cfc04e
Marija <marijavecovska99@gmail.com>
2024-03-29 04:16:35 +0100

    "Renamed schemaorg to schema in generated examples"

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 
 setup(
     name='rdf_shacl_generator',
-    version='1.0.6',
+    version='1.0.7',
     description = 'Synthetic RDF graph generator based on SHACL constraints.',
     long_description = description,
     long_description_content_type = 'text/markdown',
